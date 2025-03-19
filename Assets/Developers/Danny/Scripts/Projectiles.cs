d50fd8695801468d1367f35576427ffe012383c2
Acc-DDV <danny.devreugd@hotmail.com>
2025-03-19 12:41:19 +0100

    update
    scripts toegevoegd

---
+++ Assets/Developers/Danny/Scripts/Projectiles.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class Projectiles : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+    protected virtual void Fire()
+    {
+
+    }
+
+    
+}
