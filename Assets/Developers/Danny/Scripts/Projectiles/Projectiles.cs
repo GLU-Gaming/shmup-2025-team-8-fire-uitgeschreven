54b04340e5090d4955b6dfd93fe58b338f1c04d0
Acc-DDV <danny.devreugd@hotmail.com>
2025-03-21 11:14:59 +0100

    removed obstacle

--- Assets/Developers/Danny/Scripts/Projectiles/Projectiles.cs
+++ Assets/Developers/Danny/Scripts/Projectiles/Projectiles.cs
@@ -4,13 +4,12 @@ public class Projectiles : MonoBehaviour
 {
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.CompareTag("Player") || collision.gameObject.CompareTag("Obstacle") || collision.gameObject.CompareTag("Border"))
+        if (collision.gameObject.CompareTag("Player") || collision.gameObject.CompareTag("Border"))
         {
 
         }
         else 
         {
-
             Destroy(collision.gameObject);
         }
         Destroy(gameObject);
