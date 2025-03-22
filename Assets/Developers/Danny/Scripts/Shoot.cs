3d75fc7dea28658b84e35094e5f779100ededef1
Acc-DDV <danny.devreugd@hotmail.com>
2025-03-22 12:45:55 +0100

    Wapens swappen + harpoon schieten

--- Assets/Developers/Danny/Scripts/Shoot.cs
+++ Assets/Developers/Danny/Scripts/Shoot.cs
@@ -4,36 +4,81 @@ using System.Collections;
 
 public class Shoot : MonoBehaviour
 {
-
+    //projectiles and spawnpoints
     [SerializeField] private GameObject bullet1;
     [SerializeField] private GameObject harpoon1;
-    [SerializeField] private GameObject spawnPoint;
+    [SerializeField] private GameObject bulletSpawnPoint;
+    [SerializeField] private GameObject harpoonSpawnPoint;
 
     private GameObject bullet2;
     private GameObject harpoon2;
 
+    //projectile rigidbody
     private Rigidbody bulletBody;
     private Rigidbody harpoonBody;
 
-    private float timer;
+    //projectile speed and current weapon
     [SerializeField] private float bulletSpeed;
-    void Start()
-    {
-    }
+    private float currentWeapon = 0;
+    //timer
+    private float timer;
+    private float weaponTimer;
+
     void FixedUpdate()
     {
+        //increase the timer each frame
         timer += Time.deltaTime;
-        if(Input.GetKey("space"))
-        {
-            if(timer >= 0.1f)
-            {
+        weaponTimer += Time.deltaTime;
 
-                bullet2 = Instantiate(bullet1, spawnPoint.transform.position, spawnPoint.transform.rotation);
-                bulletBody = bullet2.GetComponent<Rigidbody>();
-                bulletBody.AddForce(bullet2.transform.forward*bulletSpeed*Time.deltaTime);
-                timer = 0;
+        //swap weapon
+        if (Input.GetKey(KeyCode.Q))
+        {
+            //change the current weapon value
+            if (weaponTimer >= 1f)
+            {
+                if (currentWeapon == 0)
+                {
+                    currentWeapon = 1;
+                }
+                else if (currentWeapon == 1)
+                {
+                    currentWeapon = 0;
+                }
+
+                print(currentWeapon);
+            }
+            weaponTimer = 0;
+        }
+        //is the shoot buttin pressed
+        if (Input.GetKey("space"))
+        {   
+            //regular bullet
+            if (currentWeapon == 0)
+            {
+                //bullet cooldown
+                if (timer >= 0.1f)
+                {
+                    shootWeapon(bullet1, bullet2, bulletBody, bulletSpawnPoint);
+                }
+            }
+            //harpoon
+            else if (currentWeapon == 1)
+            {
+                //harpoon cooldown
+                if (timer >= 1f)
+                {
+                    shootWeapon(harpoon1, harpoon2, harpoonBody, harpoonSpawnPoint);
+                }
             }
         }
     }
-
+    //function to fire the projectiles
+    private void shootWeapon(GameObject weaponPrefab, GameObject newWeapon, Rigidbody rigidbody, GameObject spawnpoint)
+    {
+        newWeapon = Instantiate(weaponPrefab, spawnpoint.transform.position, spawnpoint.transform.rotation);
+        rigidbody = newWeapon.GetComponent<Rigidbody>();
+        rigidbody.AddForce(newWeapon.transform.forward * bulletSpeed);
+        //reset timer
+        timer = 0;
+    }
 }
