0ace1588b0929181eceae3e81df9ac0e497a0c2a
nidjn246 <milanveldhuizen09@gmail.com>
2025-04-02 12:38:21 +0200

    BAckground added and bossfight finished and updated main

--- Assets/Developers/Milan/Scripts/DepthMeter.cs
+++ Assets/Developers/Milan/Scripts/DepthMeter.cs
@@ -1,4 +1,3 @@
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class DepthMeter : MonoBehaviour
@@ -19,12 +18,9 @@ public class DepthMeter : MonoBehaviour
     {
         if (game.isWaveCleared == true)
         {
-            smallPlayer.transform.rotation = Quaternion.Slerp(smallPlayer.transform.rotation, Quaternion.Euler(0, 0, -90), 0.3f);
+
             smallPlayer.transform.position = new Vector3(smallPlayer.transform.position.x, smallPlayer.transform.position.y - 0.0065f, smallPlayer.transform.position.z);
         }
-        else
-        {
-            smallPlayer.transform.rotation = Quaternion.Slerp(smallPlayer.transform.rotation, Quaternion.Euler(0, 0, 0), 0.3f);
-        }
+
     }
 }
