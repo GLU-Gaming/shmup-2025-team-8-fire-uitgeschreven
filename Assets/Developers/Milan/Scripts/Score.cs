a9538adbab5637bc543e4bc4c691df194618cbc3
nidjn246 <milanveldhuizen09@gmail.com>
2025-04-09 21:48:06 +0200

    background music

--- Assets/Developers/Milan/Scripts/Score.cs
+++ Assets/Developers/Milan/Scripts/Score.cs
@@ -1,10 +1,12 @@
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Score : MonoBehaviour
 {
     public int amountScore;
     [SerializeField] private TextMeshProUGUI scoreText;
+    private GameObject GameObjectScore;
     void Start()
     {
 
@@ -13,5 +15,12 @@ public class Score : MonoBehaviour
     void Update()
     {
         scoreText.text = "Score: " + amountScore;
+        if (SceneManager.GetActiveScene().name == "Win Screen" || SceneManager.GetActiveScene().name == "Game Over")
+        {
+            GameObjectScore = GameObject.Find("Score");
+            scoreText = GameObjectScore.GetComponent<TextMeshProUGUI>();
+            scoreText.text = "Score: " + amountScore;
+            Destroy(gameObject);
+        }
     }
 }
